6514506cf3c6eb9af8a2fe1a2cb239ece111069c
Sushil Thapa <thapasushil6@gmail.com>
2024-06-16 09:57:29 +0545

    chore: add package, refactor

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 function capitalizeText(string) {
-  if (typeof string !== 'string') return '';
-  return string.charAt(0).toUpperCase() + string.slice(1);
+  if (typeof string !== 'string') return ''
+  return string.charAt(0).toUpperCase() + string.slice(1)
 }
 
 function reverseString(string) {
-  if (typeof string !== 'string') return '';
-  return string.split('').reverse().join('');
+  if (typeof string !== 'string') return ''
+  return string.split('').reverse().join('')
+}
+
+function stringLen(string) {
+  if (typeof string !== 'string') return 0
+  return string.length
 }
 
 module.exports = {
   capitalizeText,
   reverseString,
-};
+  stringLen
+}
