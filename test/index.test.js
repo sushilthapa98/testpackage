6edf56beb6ccd574c0f2e64087f008acdb5de1fd
Sushil Thapa <thapasushil6@gmail.com>
2024-06-16 10:11:13 +0545

    add test for string lenth

--- test/index.test.js
+++ test/index.test.js
@@ -1,9 +1,13 @@
-const { capitalizeText, reverseString } = require('../src/index');
+const { capitalizeText, reverseString, stringLen } = require('../src/index')
 
 test('capitalizes the first letter of string', () => {
-  expect(capitalizeText('hello')).toBe('Hello');
-});
+  expect(capitalizeText('hello')).toBe('Hello')
+})
 
 test('reverses a string', () => {
-  expect(reverseString('hello')).toBe('olleh');
-});
+  expect(reverseString('hello')).toBe('olleh')
+})
+
+test('generates string length', () => {
+  expect(stringLen('hello')).toBe(5)
+})
